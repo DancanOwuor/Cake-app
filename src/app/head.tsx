bad241edecb3f25d837a1eca27127441e66cbf35
Dancan Owuor <duncanowuor2@gmail.com>
2025-06-06 20:51:24 +0300

    updating head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,8 @@
 export default function Head() {
   return (
     <>
-      <meta
-        name="google-site-verification"
-        content="googlecd606b08142bdb04.html"
-      />
+      <meta name="google-site-verification" 
+      content="sdUfaykdt5-RJX0eYWXA2Ybdg0tZmj99AIpztKcnEuo" />
     </>
   );
 }
